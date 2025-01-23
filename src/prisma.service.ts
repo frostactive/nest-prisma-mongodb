66844090ea814e6af731e4ba26e8812568590495
Abiola Fajimi <frostactive@gmail.com>
2025-01-23 11:25:21 -0500

    commit #6

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -4,6 +4,8 @@ import { PrismaClient } from '@prisma/client';
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
+    const print = `The connection URL is ${process.env.DATABASE_URL.substring(40)}`
+    console.error(print)
     await this.$connect();
   }
 }
