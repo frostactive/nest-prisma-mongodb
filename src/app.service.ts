fde749738c2b10df0e8991c042e8c09578eace83
Abiola Fajimi <frostactive@gmail.com>
2025-01-23 11:16:08 -0500

    commit #5

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello Worldwide!';
+    return `The connection URL is ${process.env.DATABASE_URL}`;
   }
 }
